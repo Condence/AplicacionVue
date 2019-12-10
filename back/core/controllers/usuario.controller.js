ea9799583a8238b7d56fe275f1e6c23eece46646
David Conde <conde@MacBook-Pro-de-David.local>
2019-12-09 23:38:51 -0700

    back

---
+++ back/core/controllers/usuario.controller.js
@@ -0,0 +1,65 @@
+const usuarioDAO = require("../persistence/dao/usuario.dao");
+const config = require("../../config");
+var bcrypt = require('bcrypt'); 
+
+module.exports = {
+    getUsuarios(req, res) {        
+        try{
+            usuarioDAO.getUsuarios().then((result)=>{
+                res.status(200).json(result);
+            }).catch((error)=>{
+                res.status(500).json(error);
+            });  
+        }catch(error){
+            res.status(500).json(error);
+        }                     
+    },
+    getUsuario(req, res){
+        let idUsuario = req.params.idUsuario;
+        usuarioDAO.getUsuario(idUsuario).then((result)=>{
+            res.status(200).json(result);
+        }).catch((error)=>{
+            res.status(500).json(error);
+        });        
+    },
+    getName(req, res){
+        let idUsuario = req.params.idUsuario;
+        usuarioDAO.getName(idUsuario).then((result)=>{
+            res.status(200).json(result);
+        }).catch((error)=>{
+            res.status(500).json(error);
+        });        
+    },
+    postUsuario(req, res){
+        let usuario = req.body;  
+        //var salt = bcrypt.genSaltSync(config.auth.rounds);
+        //var hash = bcrypt.hashSync(usuario.password, salt);
+        //usuario.password = hash;
+        usuarioDAO.postUsuario(usuario).then((result)=>{
+            res.status(200).json(result);
+        }).catch((error)=>{
+            res.status(500).json(error);
+        });        
+    },
+    putUsuario(req, res){
+        let usuario = req.body;
+        let idUsuario = req.params.idUsuario; 
+        usuarioDAO.putUsuario(idUsuario, usuario).then((result)=>{
+            res.status(200).json(result);
+        }).catch((error)=>{
+            res.status(500).json(error);
+        });   
+    },
+    deleteUsuario(req, res){
+        let idUsuario = req.params.idUsuario;
+        try{
+            usuarioDAO.deleteUsuario(idUsuario).then((result)=>{
+                res.status(200).json("Usuario eliminado");
+            }).catch((error)=>{
+                res.status(500).json(error);
+            });  
+        }catch(error){
+            res.status(500).json(error);
+        }
+    }
+};
---
+++ back/core/persistence/dao/Auth.dao.js
@@ -0,0 +1,36 @@
+const usuarioModel = require("../schemas/usuario.schema");
+var jwt = require('jsonwebtoken');
+const config = require("../../../config");
+var bcrypt = require('bcrypt'); 
+const tokens = require("../../../middleware/tokens");
+
+module.exports.login = function(body_correo, body_password){
+    return new Promise((resolve, reject) => {
+        usuarioModel.findOne({'correo': body_correo}, (error, result)=>{
+            if(error){
+                reject("Trono: correo no encontrado: " + error);
+            }else{ 
+                if(result){
+                    if(result.password == body_password){   
+                        const user = {id: result.id};
+                        const roles = {id: result.rol};
+                        const nombrer = result.nombre;
+                        const token = tokens.generateToken(user, nombrer, roles, body_correo); 
+                        resolve({
+                            data:{
+                                nombre: nombrer,
+                                rol:roles,
+                                correo: body_correo,
+                                token: token
+                            }
+                        });
+                    } else { 
+                        reject("Trono: contraseña erronea " + error);
+                    } 
+                } else {
+                    reject("Trono: no se encontro correo: " + error);
+                }
+            }
+        }); 
+    });
+}
